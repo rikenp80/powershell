c657ea0e92f16e936e34816c7bc604ce9d50d92c
rikenp80 <79237821+rikenp80@users.noreply.github.com>
2022-04-21 08:32:29 -0400

    new file

---
+++ RecurseFileSearch.ps1
@@ -0,0 +1,17 @@
+﻿cls
+$source_path =  ""
+
+$files = dir $source_path -recurse | where {$_.extension -in ".jpg",".png" -and $_.PsIsContainer -eq $false } | Select Fullname -ExpandProperty Fullname
+
+#$files = gci -Recurse $source_path -Filter *.jpg, *.png | Select Fullname -ExpandProperty Fullname
+#$files
+
+New-Item -Path $source_path -Name "new" -ItemType "directory"
+
+$target_path =  $source_path + "\new\"
+
+foreach($file in $files)
+{    
+   write-output $file
+   Copy-Item -Path $file -Destination $target_path
+}
