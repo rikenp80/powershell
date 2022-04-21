ea7bd413155e61a0d5d40b7d02e19013b35e3be7
rikenp80 <79237821+rikenp80@users.noreply.github.com>
2022-04-21 08:31:27 -0400

    new script

---
+++ randompassword.ps1
@@ -0,0 +1,18 @@
+﻿cls
+$count = 1
+
+while ($count -le 6) #specify number of passwords that need to be generated
+{
+$password = (("123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz".tochararray() | sort {Get-Random})[0..15] -join '')
+
+$password
+$count = $count + 1
+}
+
+
+
+#$password = (("123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz".tochararray() | sort {Get-Random})[0..15] -join '')
+#$password
+
+#$arr = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz".tochararray()
+#(Get-Random -Count 20 -InputObject $arr) -join ''
